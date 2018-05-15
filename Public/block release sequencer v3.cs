19737cd708d73b3091fbfef4ddf5232bc5e39d18
Josh <whiplash141@gmail.com>
2018-05-15 14:01:04 -0500

    Add files via upload

---
+++ Public/block release sequencer v3.cs
@@ -0,0 +1,116 @@
+/*   
+/// Whip's Merge/Rotor/Connector Sequenced Release Script v3 - revision: 5/15/18 ///
+
+- Whiplash141
+*/
+
+//---You can modify these
+string releaseGroupName = "Release";
+const int framesBetweenReleases = 30; //delay between releases in frames (60 = 1 sec)    
+
+//---Don't touch these
+int currentFrame = 0;
+int releaseIndex = 0;
+bool shouldTrigger = false;
+List<IMyTerminalBlock> releaseBlocks = new List<IMyTerminalBlock>();
+
+void Main(string arg, UpdateType updateSource)
+{
+    if (arg.ToLower() == "release" && !shouldTrigger)
+    {
+        if (GetBlocks())
+        {
+            currentFrame = framesBetweenReleases;
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+            shouldTrigger = true;
+        }
+        else
+        {
+            Echo("Release sequence cancelled...");
+            return;
+        }
+    }
+
+    if ((updateSource & UpdateType.Update1) != 0)
+    {
+        if (currentFrame % framesBetweenReleases == 0)
+        {
+            SequenceRelease();
+            currentFrame = 0;
+        }
+        currentFrame++;
+
+        Echo("Releasing: " + shouldTrigger.ToString());
+        Echo("Current Frame: " + currentFrame);
+        Echo("Max Frame: " + framesBetweenReleases);
+        Echo("Current Index: " + releaseIndex);
+        Echo("No. of Merges: " + releaseBlocks.Count.ToString());
+    }
+}
+
+bool GetBlocks()
+{
+    var group = GridTerminalSystem.GetBlockGroupWithName(releaseGroupName);
+    if (group == null)
+    {
+        Echo($"Error: No block group named '{releaseGroupName}'");
+        return false;
+    }
+
+    group.GetBlocks(releaseBlocks, x => x is IMyMotorStator || x is IMyShipConnector || x is IMyShipMergeBlock);
+
+    if (releaseBlocks.Count == 0)
+    {
+        Echo($"Error: No merges, connectors, or rotors found in '{releaseGroupName}' group");
+        return false;
+    }
+
+    return true;
+}
+
+void SequenceRelease()
+{
+    if (releaseIndex < releaseBlocks.Count)
+    {
+        var block = releaseBlocks[releaseIndex];
+
+        var merge = block as IMyShipMergeBlock;
+        if (merge != null)
+            merge.Enabled = false;
+
+        var rotor = block as IMyMotorStator;
+        if (rotor != null)
+            rotor.Detach();
+
+        var connector = block as IMyShipConnector;
+        if (connector != null)
+            connector.Disconnect();
+
+        releaseIndex++;
+    }
+    else
+    {
+        Runtime.UpdateFrequency = UpdateFrequency.None;
+        releaseIndex = 0;
+        shouldTrigger = false;
+        ResetReleaseBlocks();
+    }
+}
+
+void ResetReleaseBlocks()
+{
+    foreach (var block in releaseBlocks)
+    {
+        var merge = block as IMyShipMergeBlock;
+        if (merge != null)
+            merge.Enabled = true;
+
+        var rotor = block as IMyMotorStator;
+        if (rotor != null)
+            rotor.Attach();
+
+        var connector = block as IMyShipConnector;
+        if (connector != null)
+            connector.Connect();
+    }
+}
