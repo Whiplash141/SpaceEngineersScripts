849043c6fc95f881cf88ded3426addc6766e3ed8
Josh <whiplash141@gmail.com>
2018-06-21 21:16:48 -0500

    Add files via upload

---
+++ Public/class_VectorMath.cs
@@ -0,0 +1,61 @@
+class VectorMath
+{
+    /// <summary>
+    /// Reflects vector a over vector b with an optional rejection factor
+    /// </summary>
+    public static Vector3D Reflection(Vector3D a, Vector3D b, double rejectionFactor = 1) //reflect a over b
+    {
+        Vector3D project_a = Projection(a, b);
+        Vector3D reject_a = a - project_a;
+        return project_a - reject_a * rejectionFactor;
+    }
+
+    /// <summary>
+    /// Rejects vector a on vector b
+    /// </summary>
+    public static Vector3D Rejection(Vector3D a, Vector3D b) //reject a on b
+    {
+        if (Vector3D.IsZero(b))
+            return Vector3D.Zero;
+
+        return a - a.Dot(b) / b.LengthSquared() * b;
+    }
+
+    /// <summary>
+    /// Projects vector a onto vector b
+    /// </summary>
+    public static Vector3D Projection(Vector3D a, Vector3D b)
+    {
+        if (Vector3D.IsZero(b))
+            return Vector3D.Zero;
+
+        return a.Dot(b) / b.LengthSquared() * b;
+    }
+
+    /// <summary>
+    /// Scalar projection of a onto b
+    /// </summary>
+    public static double ScalarProjection(Vector3D a, Vector3D b)
+    {
+        if (Vector3D.IsZero(b))
+            return 0;
+
+        if (Vector3D.IsUnit(ref b))
+            return a.Dot(b);
+
+        return a.Dot(b) / b.Length();
+    }
+
+    /// <summary>
+    /// Computes angle between 2 vectors
+    /// </summary>
+    public static double AngleBetween(Vector3D a, Vector3D b, bool useSmallestAngle = false) //returns radians
+    {
+        if (Vector3D.IsZero(a) || Vector3D.IsZero(b))
+            return 0;
+        else if (useSmallestAngle)
+            return Math.Acos(MathHelper.Clamp(Math.Abs(a.Dot(b)) / Math.Sqrt(a.LengthSquared() * b.LengthSquared()), -1, 1));
+        else
+            return Math.Acos(MathHelper.Clamp(a.Dot(b) / Math.Sqrt(a.LengthSquared() * b.LengthSquared()), -1, 1));
+    }
+}
