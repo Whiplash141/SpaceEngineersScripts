6d41f6a3f33b3eafd52945678dbae1f40da643f2
Josh <whiplash141@gmail.com>
2019-07-05 14:56:13 -0500

    Update and rename class_VectorMath.cs to VectorMath.cs

--- Public/VectorMath.cs
+++ Public/VectorMath.cs
@@ -1,16 +1,19 @@
-class VectorMath
+public static class VectorMath
 {
     /// <summary>
-    ///  Normalizes a vector only if it is non-zero
+    ///  Normalizes a vector only if it is non-zero and non-unit
     /// </summary>
     public static Vector3D SafeNormalize(Vector3D a)
     {
         if (Vector3D.IsZero(a))
+            return Vector3D.Zero;
+
+        if (Vector3D.IsUnit(ref a))
             return a;
 
         return Vector3D.Normalize(a);
     }
-    
+
     /// <summary>
     /// Reflects vector a over vector b with an optional rejection factor
     /// </summary>
@@ -39,6 +42,9 @@ class VectorMath
     {
         if (Vector3D.IsZero(a) || Vector3D.IsZero(b))
             return Vector3D.Zero;
+        
+        if (Vector3D.IsUnit(ref b))
+            return a.Dot(b) * b;
 
         return a.Dot(b) / b.LengthSquared() * b;
     }
@@ -58,24 +64,39 @@ class VectorMath
     }
 
     /// <summary>
-    /// Computes angle between 2 vectors
+    /// Computes angle between 2 vectors in radians.
     /// </summary>
-    public static double AngleBetween(Vector3D a, Vector3D b) //returns radians
+    public static double AngleBetween(Vector3D a, Vector3D b)
     {
         if (Vector3D.IsZero(a) || Vector3D.IsZero(b))
             return 0;
         else
             return Math.Acos(MathHelper.Clamp(a.Dot(b) / Math.Sqrt(a.LengthSquared() * b.LengthSquared()), -1, 1));
     }
-    
+
     /// <summary>
-    /// Computes cosine of the angle between 2 vectors
+    /// Computes cosine of the angle between 2 vectors.
     /// </summary>
-    public static double CosBetween(Vector3D a, Vector3D b, bool useSmallestAngle = false) //returns radians
+    public static double CosBetween(Vector3D a, Vector3D b)
     {
         if (Vector3D.IsZero(a) || Vector3D.IsZero(b))
             return 0;
         else
             return MathHelper.Clamp(a.Dot(b) / Math.Sqrt(a.LengthSquared() * b.LengthSquared()), -1, 1);
+    }
+
+    /// <summary>
+    /// Returns if the normalized dot product between two vectors is greater than the tolerance.
+    /// This is helpful for determining if two vectors are "more parallel" than the tolerance.
+    /// </summary>
+    /// <param name="a">First vector</param>
+    /// <param name="b">Second vector</param>
+    /// <param name="tolerance">Cosine of maximum angle</param>
+    /// <returns></returns>
+    public static bool IsDotProductWithinTolerance(Vector3D a, Vector3D b, double tolerance)
+    {
+        double dot = Vector3D.Dot(a, b);
+        double num = a.LengthSquared() * b.LengthSquared() * tolerance * Math.Abs(tolerance);
+        return Math.Abs(dot) * dot > num;
     }
 }
