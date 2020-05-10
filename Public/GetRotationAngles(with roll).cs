a3ee002f8fb4d3070c1ce498820970c248501cdb
Josh <whiplash141@gmail.com>
2020-05-09 20:24:36 -0500

    Update GetRotationAngles(with roll).cs

--- Public/GetRotationAngles(with roll).cs
+++ Public/GetRotationAngles(with roll).cs
@@ -8,21 +8,21 @@ void GetRotationAngles(Vector3D desiredForwardVector, Vector3D desiredUpVector, 
     var localTargetVector = Vector3D.Rotate(desiredForwardVector, MatrixD.Transpose(worldMatrix));
     var flattenedTargetVector = new Vector3D(localTargetVector.X, 0, localTargetVector.Z);
 
-    int yawSign = Math.Sign(localTargetVector.X);
-    if (yawSign == 0) { // Fixes sign for straight back case
-        yawSign = 1;
-    }
+    int yawSign = localTargetVector.X >= 0 ? 1 : -1;
     yaw = VectorMath.AngleBetween(Vector3D.Forward, flattenedTargetVector) * yawSign; //right is positive
 
     int pitchSign = Math.Sign(localTargetVector.Y);
-    if (Vector3D.IsZero(flattenedTargetVector)) { //check for straight up case
+    if (Vector3D.IsZero(flattenedTargetVector))
+    { //check for straight up case
         pitch = MathHelper.PiOver2 * pitchSign;
     }
-    else {
+    else
+    {
         pitch = VectorMath.AngleBetween(localTargetVector, flattenedTargetVector) * pitchSign; //up is positive
     }
 
-    if (Vector3D.IsZero(desiredUpVector)) {
+    if (Vector3D.IsZero(desiredUpVector))
+    {
         roll = 0;
         return;
     }
@@ -30,35 +30,47 @@ void GetRotationAngles(Vector3D desiredForwardVector, Vector3D desiredUpVector, 
     // Since there is a relationship between roll and the orientation of forward
     // we need to ensure that the up we are comparing is orthagonal to forward.
     Vector3D orthagonalUp;
-    if (Vector3D.Dot(desiredForwardVector, desiredUpVector) == 0) {
+    Vector3D orthagonalLeft = Vector3D.Cross(desiredUpVector, desiredForwardVector);
+    if (Vector3D.Dot(desiredForwardVector, desiredUpVector) == 0)
+    {
         orthagonalUp = desiredUpVector;
-    } else {
-        var intermediateLeft = Vector3D.Cross(desiredUpVector, desiredForwardVector);
-        orthagonalUp = Vector3D.Cross(desiredForwardVector, intermediateLeft);
+    }
+    else
+    {
+        orthagonalUp = Vector3D.Cross(desiredForwardVector, orthagonalLeft);
     }
 
     var localUpVector = Vector3D.Rotate(orthagonalUp, MatrixD.Transpose(worldMatrix));
-    var flattenedUpVector = new Vector3D(localUpVector.X, localUpVector.Y, 0);
-    int rollSign = Math.Sign(flattenedUpVector.X);
-    if (rollSign == 0) {
-        rollSign = 1;
+    int signRoll = Vector3D.Dot(localUpVector, Vector3D.Right) >= 0 ? 1 : -1;
+
+    // We are going to try and construct new intermediate axes where:
+    // intermediateUp = Vector3D.Up
+    // intermediateFront = flattenedTargetVector
+    //
+    // If flattenedTargetVector is zero, that means we are pointing either straight up
+    // or straight down.
+    Vector3D intermediateFront = flattenedTargetVector;
+    if (Vector3D.IsZero(flattenedTargetVector)) 
+    {
+        // Desired forward and current up are parallel
+        // This implies pitch is ±90° and yaw is 0.
+        var localUpVectorFlattenedY = new Vector3D(localUpVector.X, 0, localUpVector.Z);
+
+        // If straight up, reference direction would be backward,
+        // if straight down, reference direction would be forward.
+        // This is because we are simply doing a ±90° pitch rotation
+        // of the axes.
+        var referenceDirection = Vector3D.Dot(Vector3D.Up, localTargetVector) >= 0 ? Vector3D.Backward : Vector3D.Forward;
+        
+        roll = VectorMath.AngleBetween(localUpVectorFlattenedY, referenceDirection) * signRoll;
+        return;
     }
-    if (Vector3D.IsZero(flattenedUpVector)) {
-        var sign = Math.Sign(localTargetVector.X) * Math.Sign(localUpVector.Z);
-        if (sign == 0) {
-            var sign2 = Math.Sign(localTargetVector.Y) * Math.Sign(localUpVector.Z);
-            if (sign2 < 0) {
-                roll = Math.PI;
-            } else {
-                roll = 0;
-            }
-        } else {
-            roll = MathHelper.PiOver2 * sign;
-        }
-    } else {
-        roll = VectorMath.AngleBetween(flattenedUpVector, Vector3D.Up) * rollSign;
-        if (localTargetVector.Z > 0) {
-            roll *= -1;
-        }
-    }
+
+    // Flatten up vector onto the intermediate up-right plane
+    var localUpProjOnIntermediateForward = Vector3D.Dot(intermediateFront, localUpVector) / intermediateFront.LengthSquared() * intermediateFront;
+    var flattenedUpVector = localUpVector - localUpProjOnIntermediateForward;
+
+    var intermediateRight = Vector3D.Cross(intermediateFront, Vector3D.Up);
+    int rollSign = Vector3D.Dot(flattenedUpVector, intermediateRight) >= 0 ? 1 : -1;
+    roll = VectorMath.AngleBetween(flattenedUpVector, Vector3D.Up) * rollSign;
 }
