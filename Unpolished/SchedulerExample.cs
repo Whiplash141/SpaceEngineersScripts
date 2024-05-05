67c5f93148c1f8a4447e433d21ed1286bcad49a9
Josh <whiplash141@gmail.com>
2024-05-05 16:42:44 -0500

    Create SchedulerExample.cs

---
+++ Unpolished/SchedulerExample.cs
@@ -0,0 +1,275 @@
+
+// Declare a scheduler
+Scheduler _scheduler;
+
+// Program constructor that is run when the script is compiled for the first time
+Program()
+{
+    // Lets instantiate our scheduler within our constructor
+    _scheduler = new Scheduler(this);
+
+    _scheduler.AddScheduledAction(MethodIWantToSchedule, 3.0); // This will run 3 times a second
+
+    _scheduler.AddScheduledAction(PrintMethod, 1.0); // This will run one time a second
+
+    Runtime.UpdateFrequency = UpdateFrequency.Update10; 
+    // The valid cyclical options are 1, 10, and 100.
+    // Prefer Update10 (6Hz) or Update100 (0.67 Hz) if you can since it will lower the overhead
+    // of running this program.
+    // See also: https://github.com/malware-dev/MDK-SE/wiki/Continuous-Running-No-Timers-Needed
+}
+
+int _counter = 0;
+void MethodIWantToSchedule()
+{
+    _counter++;
+}
+
+void PrintMethod()
+{
+    Echo($"Counter: {_counter}");
+}
+
+void Main(string arg, UpdateType src)
+{
+    // This automagically updates any of the things you have scheduled based
+    // on the frequencies that you specified.
+    // What we should see is every second, our count should increase by 3
+    _scheduler.Update();
+}
+
+#region Scheduler
+/// <summary>
+/// Class for scheduling actions to occur at specific frequencies. Actions can be updated in parallel or in sequence (queued).
+/// </summary>
+public class Scheduler
+{
+    public double CurrentTimeSinceLastRun { get; private set; } = 0;
+    public long CurrentTicksSinceLastRun { get; private set; } = 0;
+
+    QueuedAction _currentlyQueuedAction = null;
+    bool _firstRun = true;
+    bool _inUpdate = false;
+
+    readonly bool _ignoreFirstRun;
+    readonly List<ScheduledAction> _actionsToAdd = new List<ScheduledAction>();
+    readonly List<ScheduledAction> _scheduledActions = new List<ScheduledAction>();
+    readonly List<ScheduledAction> _actionsToDispose = new List<ScheduledAction>();
+    readonly Queue<QueuedAction> _queuedActions = new Queue<QueuedAction>();
+    readonly Program _program;
+
+    public const long TicksPerSecond = 60;
+    public const double TickDurationSeconds = 1.0 / TicksPerSecond;
+    const long ClockTicksPerGameTick = 166666L;
+
+    /// <summary>
+    /// Constructs a scheduler object with timing based on the runtime of the input program.
+    /// </summary>
+    public Scheduler(Program program, bool ignoreFirstRun = false)
+    {
+        _program = program;
+        _ignoreFirstRun = ignoreFirstRun;
+    }
+
+    /// <summary>
+    /// Updates all ScheduledAcions in the schedule and the queue.
+    /// </summary>
+    public void Update()
+    {
+        _inUpdate = true;
+        long deltaTicks = Math.Max(0, _program.Runtime.TimeSinceLastRun.Ticks / ClockTicksPerGameTick);
+
+        if (_firstRun)
+        {
+            if (_ignoreFirstRun)
+            {
+                deltaTicks = 0;
+            }
+            _firstRun = false;
+        }
+
+        _actionsToDispose.Clear();
+        foreach (ScheduledAction action in _scheduledActions)
+        {
+            CurrentTicksSinceLastRun = action.TicksSinceLastRun + deltaTicks;
+            CurrentTimeSinceLastRun = action.TimeSinceLastRun + deltaTicks * TickDurationSeconds;
+            action.Update(deltaTicks);
+            if (action.JustRan && action.DisposeAfterRun)
+            {
+                _actionsToDispose.Add(action);
+            }
+        }
+
+        if (_actionsToDispose.Count > 0)
+        {
+            _scheduledActions.RemoveAll((x) => _actionsToDispose.Contains(x));
+        }
+
+        if (_currentlyQueuedAction == null)
+        {
+            // If queue is not empty, populate current queued action
+            if (_queuedActions.Count != 0)
+                _currentlyQueuedAction = _queuedActions.Dequeue();
+        }
+
+        // If queued action is populated
+        if (_currentlyQueuedAction != null)
+        {
+            _currentlyQueuedAction.Update(deltaTicks);
+            if (_currentlyQueuedAction.JustRan)
+            {
+                if (!_currentlyQueuedAction.DisposeAfterRun)
+                {
+                    _queuedActions.Enqueue(_currentlyQueuedAction);
+                }
+                // Set the queued action to null for the next cycle
+                _currentlyQueuedAction = null;
+            }
+        }
+        _inUpdate = false;
+
+        if (_actionsToAdd.Count > 0)
+        {
+            _scheduledActions.AddRange(_actionsToAdd);
+            _actionsToAdd.Clear();
+        }
+    }
+
+    /// <summary>
+    /// Adds an Action to the schedule. All actions are updated each update call.
+    /// </summary>
+    public void AddScheduledAction(Action action, double updateFrequency, bool disposeAfterRun = false, double timeOffset = 0)
+    {
+        ScheduledAction scheduledAction = new ScheduledAction(action, updateFrequency, disposeAfterRun, timeOffset);
+        if (!_inUpdate)
+            _scheduledActions.Add(scheduledAction);
+        else
+            _actionsToAdd.Add(scheduledAction);
+    }
+
+    /// <summary>
+    /// Adds a ScheduledAction to the schedule. All actions are updated each update call.
+    /// </summary>
+    public void AddScheduledAction(ScheduledAction scheduledAction)
+    {
+        if (!_inUpdate)
+            _scheduledActions.Add(scheduledAction);
+        else
+            _actionsToAdd.Add(scheduledAction);
+    }
+
+    /// <summary>
+    /// Adds an Action to the queue. Queue is FIFO.
+    /// </summary>
+    public void AddQueuedAction(Action action, double updateInterval, bool removeAfterRun = false)
+    {
+        if (updateInterval <= 0)
+        {
+            updateInterval = 0.001; // avoids divide by zero
+        }
+        QueuedAction scheduledAction = new QueuedAction(action, updateInterval, removeAfterRun);
+        _queuedActions.Enqueue(scheduledAction);
+    }
+
+    /// <summary>
+    /// Adds a ScheduledAction to the queue. Queue is FIFO.
+    /// </summary>
+    public void AddQueuedAction(QueuedAction scheduledAction)
+    {
+        _queuedActions.Enqueue(scheduledAction);
+    }
+}
+
+public class QueuedAction : ScheduledAction
+{
+    public QueuedAction(Action action, double runInterval, bool removeAfterRun = false)
+        : base(action, 1.0 / runInterval, removeAfterRun: removeAfterRun, timeOffset: 0)
+    { }
+}
+
+public class ScheduledAction
+{
+    public bool JustRan { get; private set; } = false;
+    public bool DisposeAfterRun { get; private set; } = false;
+    public double TimeSinceLastRun { get { return TicksSinceLastRun * Scheduler.TickDurationSeconds; } }
+    public long TicksSinceLastRun { get; private set; } = 0;
+    public double RunInterval
+    {
+        get
+        {
+            return RunIntervalTicks * Scheduler.TickDurationSeconds;
+        }
+        set
+        {
+            RunIntervalTicks = (long)Math.Round(value * Scheduler.TicksPerSecond);
+        }
+    }
+    public long RunIntervalTicks
+    {
+        get
+        {
+            return _runIntervalTicks;
+        }
+        set
+        {
+            if (value == _runIntervalTicks)
+                return;
+
+            _runIntervalTicks = value < 0 ? 0 : value;
+            _runFrequency = value == 0 ? double.MaxValue : Scheduler.TicksPerSecond / _runIntervalTicks;
+        }
+    }
+
+    public double RunFrequency
+    {
+        get
+        {
+            return _runFrequency;
+        }
+        set
+        {
+            if (value == _runFrequency)
+                return;
+
+            if (value == 0)
+                RunIntervalTicks = long.MaxValue;
+            else
+                RunIntervalTicks = (long)Math.Round(Scheduler.TicksPerSecond / value);
+        }
+    }
+
+    long _runIntervalTicks;
+    double _runFrequency;
+    readonly Action _action;
+
+    /// <summary>
+    /// Class for scheduling an action to occur at a specified frequency (in Hz).
+    /// </summary>
+    /// <param name="action">Action to run</param>
+    /// <param name="runFrequency">How often to run in Hz</param>
+    public ScheduledAction(Action action, double runFrequency, bool removeAfterRun = false, double timeOffset = 0)
+    {
+        _action = action;
+        RunFrequency = runFrequency; // Implicitly sets RunInterval
+        DisposeAfterRun = removeAfterRun;
+        TicksSinceLastRun = (long)Math.Round(timeOffset * Scheduler.TicksPerSecond);
+    }
+
+    public void Update(long deltaTicks)
+    {
+        TicksSinceLastRun += deltaTicks;
+
+        if (TicksSinceLastRun >= RunIntervalTicks)
+        {
+            _action.Invoke();
+            TicksSinceLastRun = 0;
+
+            JustRan = true;
+        }
+        else
+        {
+            JustRan = false;
+        }
+    }
+}
+#endregion
